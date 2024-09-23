c7a05411f8515eff5feec6bbf62f92cd4146b02c
monzila-akter <monzila01727@gmail.com>
2024-09-24 04:12:03 +0600

    added buttons active function

--- features.js
+++ features.js
@@ -1,15 +1,28 @@
 
-// function call for show donation section
+const historyBtn = document.getElementById('show-history-btn');
+const donationBtn = document.getElementById('show-donation-btn');
 
+// call function for donation section
 
 document.getElementById('show-donation-btn').addEventListener('click', function(){
+    donationBtn.classList.add('text-black', "bg-lime-400", "border-none");
+    donationBtn.classList.remove('bg-gray-200', 'bg-lime-300', 'text-gray-600')
+    historyBtn.classList.remove('text-black', 'bg-lime-400');
+    historyBtn.classList.add('bg-gray-200', 'text-gray-600');
+      
     getSectionById('donation-section');
-    
-    
+       
 })
 
-// function call for show history section
 
-document.getElementById('show-history-btn').addEventListener('click', function(){
+// call function for history section  
+
+historyBtn.addEventListener('click', function(){
+    historyBtn.classList.add("text-black", "bg-lime-400", "border-none");
+    donationBtn.classList.remove('text-black', 'bg-lime-300', 'bg-lime-400');
+    donationBtn.classList.add('bg-gray-200', 'text-gray-600');
+    historyBtn.classList.remove('text-gray-600',  'bg-gray-200')
+    
+
     getSectionById('history-section');
 })
